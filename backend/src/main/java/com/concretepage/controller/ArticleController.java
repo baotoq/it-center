49c66f611558f3bc8733cfd0a528f75dd64bcd0d
Neptune <bao2703@gmail.com>
2017-11-07 15:48:35 +0700

    Up

--- backend/src/main/java/com/concretepage/controller/ArticleController.java
+++ backend/src/main/java/com/concretepage/controller/ArticleController.java
@@ -1,4 +1,5 @@
 package com.concretepage.controller;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,42 +18,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.concretepage.entity.Article;
-import com.concretepage.service.IArticleService;
+import com.concretepage.service.ArticleService;
 
 @Controller
 @RequestMapping("user")
 @CrossOrigin(origins = {"http://localhost:4200"})
 public class ArticleController {
-	@Autowired
-	private IArticleService articleService;
-	@GetMapping("article")
-	public ResponseEntity<Article> getArticleById(@RequestParam("id") String id) {
-		Article article = articleService.getArticleById(Integer.parseInt(id));
-		return new ResponseEntity<Article>(article, HttpStatus.OK);
-	}
-	@GetMapping("all-articles")
-	public ResponseEntity<List<Article>> getAllArticles() {
-		List<Article> list = articleService.getAllArticles();
-		return new ResponseEntity<List<Article>>(list, HttpStatus.OK);
-	}
-	@PostMapping("article")
-	public ResponseEntity<Void> createArticle(@RequestBody Article article, UriComponentsBuilder builder) {
-        boolean flag = articleService.createArticle(article);
-        if (flag == false) {
-        	return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+
+    @Autowired
+    private ArticleService articleService;
+
+    @GetMapping("article")
+    public ResponseEntity<Article> getArticleById(@RequestParam("id") String id) {
+        Article article = articleService.getArticleById(Integer.parseInt(id));
+        return new ResponseEntity(article, HttpStatus.OK);
+    }
+
+    @GetMapping("all-articles")
+    public ResponseEntity<List<Article>> getAllArticles() {
+        List<Article> list = articleService.getAllArticles();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+
+    @PostMapping("article")
+    public ResponseEntity<Void> createArticle(@RequestBody Article article, UriComponentsBuilder builder) {
+        if (articleService.articleExists(article.getTitle(), article.getCategory())) {
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        } else {
+            articleService.createArticle(article);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(builder.path("/article?id={id}").buildAndExpand(article.getArticleId()).toUri());
+            return new ResponseEntity(headers, HttpStatus.CREATED);
         }
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(builder.path("/article?id={id}").buildAndExpand(article.getArticleId()).toUri());
-        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
-	}
-	@PutMapping("article")
-	public ResponseEntity<Article> updateArticle(@RequestBody Article article) {
-		articleService.updateArticle(article);
-		return new ResponseEntity<Article>(article, HttpStatus.OK);
-	}
-	@DeleteMapping("article")
-	public ResponseEntity<Void> deleteArticle(@RequestParam("id") String id) {
-		articleService.deleteArticle(Integer.parseInt(id));
-		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-	}	
-} 
+    }
+
+    @PutMapping("article")
+    public ResponseEntity<Article> updateArticle(@RequestBody Article article) {
+        articleService.updateArticle(article);
+        return new ResponseEntity(article, HttpStatus.OK);
+    }
+
+    @DeleteMapping("article")
+    public ResponseEntity<Void> deleteArticle(@RequestParam("id") String id) {
+        articleService.deleteArticle(Integer.parseInt(id));
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
--- backend/src/main/java/com/concretepage/service/ArticleService.java
+++ backend/src/main/java/com/concretepage/service/ArticleService.java
@@ -1,40 +1,51 @@
 package com.concretepage.service;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.concretepage.dao.IArticleDAO;
 import com.concretepage.entity.Article;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
-public class ArticleService implements IArticleService {
-	@Autowired
-	private IArticleDAO articleDAO;
-	@Override
-	public Article getArticleById(int articleId) {
-		Article obj = articleDAO.getArticleById(articleId);
-		return obj;
-	}	
-	@Override
-	public List<Article> getAllArticles(){
-		return articleDAO.getAllArticles();
-	}
-	@Override
-	public synchronized boolean createArticle(Article article){
-       if (articleDAO.articleExists(article.getTitle(), article.getCategory())) {
-    	   return false;
-       } else {
-    	   articleDAO.createArticle(article);
-    	   return true;
-       }
-	}
-	@Override
-	public void updateArticle(Article article) {
-		articleDAO.updateArticle(article);
-	}
-	@Override
-	public void deleteArticle(int articleId) {
-		articleDAO.deleteArticle(articleId);
-	}
+@Transactional
+@Repository
+public class ArticleService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Article getArticleById(int articleId) {
+        return entityManager.find(Article.class, articleId);
+    }
+
+    @SuppressWarnings("unchecked")
+
+    public List<Article> getAllArticles() {
+        String sql = "FROM Article as atcl ORDER BY atcl.articleId DESC";
+        return (List<Article>) entityManager.createQuery(sql).getResultList();
+    }
+
+    public void createArticle(Article article) {
+        entityManager.persist(article);
+    }
+
+    public void updateArticle(Article article) {
+        Article artcl = getArticleById(article.getArticleId());
+        artcl.setTitle(article.getTitle());
+        artcl.setCategory(article.getCategory());
+        entityManager.flush();
+    }
+
+    public void deleteArticle(int articleId) {
+        entityManager.remove(getArticleById(articleId));
+    }
+
+    public boolean articleExists(String title, String category) {
+        String sql = "FROM Article as atcl WHERE atcl.title = ? and atcl.category = ?";
+        int count = entityManager.createQuery(sql).setParameter(1, title)
+                .setParameter(2, category).getResultList().size();
+        return count > 0;
+    }
 }
