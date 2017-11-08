3ddf04b1df146a15f113b2e54e5e8e70537d1d9c
Neptune <bao2703@gmail.com>
2017-11-08 17:22:49 +0700

    Added RoomResource

---
+++ backend/src/main/java/com/neptune/itcenter/resources/RoomResource.java
@@ -0,0 +1,69 @@
+package com.neptune.itcenter.resources;
+
+import com.neptune.itcenter.boms.Room;
+import com.neptune.itcenter.entities.RoomEntity;
+import com.neptune.itcenter.services.RoomService;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.List;
+
+@Stateless
+@Path("rooms")
+public class RoomResource {
+    @EJB
+    private RoomService roomService;
+
+    @Context
+    private UriInfo uriInfo;
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Room get(@PathParam("id") Integer id) {
+        RoomEntity roomEntity = roomService.findById(id);
+        return roomService.toBom(roomEntity);
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Room> getAll() {
+        List<RoomEntity> roomEntities = roomService.findAll();
+        return roomService.toBoms(roomEntities);
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response add(@Valid Room room) {
+        RoomEntity roomEntity = roomService.add(room);
+        URI createdUri = appendCurrentUriWith(String.valueOf(roomEntity.getId()));
+        return Response.created(createdUri).type(MediaType.APPLICATION_JSON).location(createdUri).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response update(@PathParam("id") Integer id, @Valid Room room) {
+        room.setId(id);
+        roomService.update(room);
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response delete(@PathParam("id") Integer id) {
+        roomService.delete(id);
+        return Response.noContent().build();
+    }
+
+    private URI appendCurrentUriWith(String fragment) {
+        return uriInfo.getAbsolutePathBuilder().path(fragment).build();
+    }
+}
--- backend/src/main/java/com/neptune/itcenter/services/RoomService.java
+++ backend/src/main/java/com/neptune/itcenter/services/RoomService.java
@@ -3,10 +3,27 @@ package com.neptune.itcenter.services;
 import com.neptune.itcenter.boms.Room;
 import com.neptune.itcenter.entities.RoomEntity;
 
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 public class RoomService extends GenericService<RoomEntity, Room> {
 
     public RoomService() {
         super(RoomEntity.class);
+    }
+
+    public List<RoomEntity> findAll() {
+        TypedQuery<RoomEntity> query = getEntityManager().createNamedQuery(RoomEntity.FIND_ALL, RoomEntity.class);
+        return query.getResultList();
+    }
+
+    public RoomEntity add(Room room) {
+        return super.add(toEntity(room));
+    }
+
+    public RoomEntity update(Room room) {
+        RoomEntity entity = findById(room.getId());
+        return super.update(entity);
     }
 
     @Override
